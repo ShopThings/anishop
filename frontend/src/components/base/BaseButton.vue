fdbb69b6ac9000684ec633819e77463824ba1d90
MMDM <mmdehghan95@gmail.com>
2023-09-03 22:27:15 +0330

    feat: add link button support

--- frontend/src/components/base/BaseButton.vue
+++ frontend/src/components/base/BaseButton.vue
@@ -1,10 +1,22 @@
 <template>
-    <button :type="type"
-            :class="[
-                'relative text-white cursor-pointer rounded-md border',
-                'py-2 px-3 text-base leading-7 transition hover:bg-opacity-90',
-            ]"
-            @click="checkButton"
+    <router-link
+        v-if="type === 'link'"
+        :class="[
+            'relative text-white cursor-pointer rounded-md border',
+            'py-2 px-3 text-base leading-7 transition hover:bg-opacity-90',
+        ]"
+        @click="checkButton"
+    >
+        <slot/>
+    </router-link>
+    <button
+        v-else
+        :type="type"
+        :class="[
+            'relative text-white cursor-pointer rounded-md border',
+            'py-2 px-3 text-base leading-7 transition hover:bg-opacity-90',
+        ]"
+        @click="checkButton"
     >
         <slot/>
     </button>
@@ -16,15 +28,15 @@ defineProps({
         type: String,
         default: 'button',
         validator(value) {
-            return ['submit', 'button', 'reset'].indexOf(value) !== -1;
+            return ['submit', 'button', 'reset', 'link'].indexOf(value) !== -1;
         },
     },
 })
 
 const emit = defineEmits(['click'])
 
-function checkButton() {
-    emit('click')
+function checkButton(e) {
+    emit('click', e)
 }
 </script>
 
