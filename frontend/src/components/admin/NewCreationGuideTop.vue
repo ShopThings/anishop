29a8510ccb86ec7e4c11a69731a261058c01a62b
MMDM <mmdehghan95@gmail.com>
2023-09-04 02:42:32 +0330

    fix: improve to prevent show with empty slots

--- frontend/src/components/admin/NewCreationGuideTop.vue
+++ frontend/src/components/admin/NewCreationGuideTop.vue
@@ -1,20 +1,30 @@
 <template>
     <div
         class="text-center mb-3 border-2 border-dashed border-violet-400 rounded-lg p-4 bg-white bg-opacity-60 sm:text-right sm:flex sm:items-center">
-        <div class="text-sm text-secondary grow mb-2 sm:mb-0">
+        <div
+            v-if="slots['text']"
+            class="text-sm text-secondary grow mb-2 sm:mb-0"
+        >
             <slot name="text"></slot>
         </div>
-        <router-link :to="{name: routeName}"
-                     class="sm:shrink-0 rounded mr-2 bg-emerald-500 text-white p-2 px-3 mx-auto flex items-center justify-center hover:bg-opacity-90 transition group text-sm">
+        <router-link
+            v-if="slots['buttonText']"
+            :to="{name: routeName}"
+            class="sm:shrink-0 rounded mr-2 bg-emerald-500 text-white p-2 px-3 mx-auto flex items-center justify-center hover:bg-opacity-90 transition group text-sm"
+        >
             <slot name="buttonText"></slot>
         </router-link>
     </div>
 </template>
 
 <script setup>
+import {useSlots} from "vue";
+
 defineProps({
     routeName: String
 })
+
+const slots = useSlots()
 </script>
 
 <style scoped>
