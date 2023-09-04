fb345a2a112ff3cb3de90447bbb4ffb12af47f18
MMDM <mmdehghan95@gmail.com>
2023-09-04 04:12:35 +0330

    feat(front): add persian date-picker package and config it

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,7 @@ import Toast, {POSITION} from "vue-toastification";
 import FloatingVue from 'floating-vue';
 import VueLazyLoad from 'vue3-lazyload';
 import Vue3ColorPicker from "vue3-colorpicker";
+import Vue3PersianDatetimePicker from "vue3-persian-datetime-picker";
 
 import "vue-toastification/dist/index.css";
 import 'floating-vue/dist/style.css';
@@ -19,6 +20,7 @@ import LayoutGuest from './components/layouts/LayoutGuest.vue';
 import LayoutUser from './components/layouts/LayoutUser.vue';
 import LayoutAdmin from './components/layouts/LayoutAdmin.vue';
 import LayoutEmpty from "./components/layouts/LayoutEmpty.vue";
+
 const pinia = createPinia()
 
 createApp(App)
@@ -28,6 +30,73 @@ createApp(App)
     .use(VueLazyLoad)
     .use(ContextMenu)
     .use(Vue3ColorPicker)
+    .use(Vue3PersianDatetimePicker, {
+        name: 'DatePicker',
+        props: {
+            dir: 'rtl',
+            clearable: true,
+            format: 'YYYY-MM-DD HH:mm',
+            displayFormat: 'jYYYY-jMM-jDD HH:mm',
+            editable: false,
+            inputClass: 'block w-full rounded-md border-0 py-3 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6',
+            placeholder: 'تاریخ مورد نظر خود را انتخاب نمایید',
+            altFormat: 'YYYY-MM-DD HH:mm',
+            color: '#3057d3',
+            autoSubmit: false,
+            compactTime: true,
+            type: 'datetime',
+            localConfig: {
+                ar: {
+                    dow: 0,
+                    dir: 'rtl',
+                    displayFormat: vm => {
+                        switch (vm.type) {
+                            case 'date':
+                                return 'YYYY/MM/DD'
+                            case 'datetime':
+                                return 'YYYY/MM/DD HH:mm'
+                            case 'year':
+                                return 'YYYY'
+                            case 'month':
+                                return 'MM'
+                            case 'yearmonth':
+                                return 'YY/MM'
+                            case 'time':
+                                return 'HH:mm'
+                        }
+                    },
+                    lang: {
+                        label: 'AR',
+                        submit: 'اختيار',
+                        cancel: 'إلغاء',
+                        now: 'الآن',
+                        nextMonth: 'الشهر القادم',
+                        prevMonth: 'الشهر الماضي'
+                    }
+                },
+                en: {
+                    dow: 0,
+                    dir: 'ltr',
+                    displayFormat: vm => {
+                        switch (vm.type) {
+                            case 'date':
+                                return 'YYYY/MM/DD'
+                            case 'datetime':
+                                return 'YYYY/MM/DD HH:mm'
+                            case 'year':
+                                return 'YYYY'
+                            case 'month':
+                                return 'MM'
+                            case 'yearmonth':
+                                return 'YY/MM'
+                            case 'time':
+                                return 'HH:mm'
+                        }
+                    }
+                }
+            },
+        }
+    })
     .use(Toast, {
         transition: 'Vue-Toastification__fade',
         rtl: true,
