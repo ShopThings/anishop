a9599cb736e96fee435cdfdd2045fabb34b823d1
MMDM <mmdehghan95@gmail.com>
2023-09-05 17:26:03 +0330

    feat: add input lead partial component

---
+++ frontend/src/components/partials/PartialInputLead.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <label
+            class="text-xs py-1 px-2 mt-1 inline-block rounded-md"
+            :class="klass"
+        >
+            {{ text }}
+        </label>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    text: String,
+    klass: {
+        type: String,
+        default: 'text-white bg-blue-500',
+    },
+})
+</script>
+
+<style scoped>
+
+</style>
