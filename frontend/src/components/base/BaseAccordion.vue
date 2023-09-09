ea8915c28b293648be5cdf515188e9fd05e42d21
MMDM <mmdehghan95@gmail.com>
2023-09-09 20:15:19 +0330

    feat: add ability to customize UI

--- frontend/src/components/base/BaseAccordion.vue
+++ frontend/src/components/base/BaseAccordion.vue
@@ -1,18 +1,25 @@
 <template>
     <Disclosure v-slot="{ open }" :default-open="open">
         <DisclosureButton
-            class="flex w-full justify-between rounded-lg bg-violet-100 px-4 py-2 text-left text-sm font-medium text-violet-900 hover:bg-violet-200 focus:outline-none focus-visible:ring focus-visible:ring-violet-500 focus-visible:ring-opacity-75 transition"
+            class="flex w-full justify-between rounded-lg px-4 py-2 text-left text-sm font-medium focus:outline-none focus-visible:ring focus-visible:ring-opacity-75 transition"
+            :class="btnClass"
         >
-            <slot name="button"></slot>
+            <slot name="button" :is-open="open"></slot>
             <ChevronUpIcon
-                :class="open ? 'rotate-0' : '-rotate-0'"
-                class="h-5 w-5 text-violet-500 transition rotate-180 transform"
+                :class="[
+                    open ? 'rotate-0' : 'rotate-180',
+                    btnIconClass,
+                ]"
+                class="h-5 w-5 text-opacity-70 transition transform"
             />
         </DisclosureButton>
 
         <VTransitionSlideFadeDownY>
-            <DisclosurePanel class="px-4 pt-4 pb-2 text-sm text-gray-500">
-                <slot name="panel"></slot>
+            <DisclosurePanel
+                class="px-4 pt-4 pb-2 text-sm text-gray-500"
+                :class="panelClass"
+            >
+                <slot name="panel" :is-open="open"></slot>
             </DisclosurePanel>
         </VTransitionSlideFadeDownY>
     </Disclosure>
@@ -28,6 +35,15 @@ defineProps({
         type: Boolean,
         default: false,
     },
+    btnClass: {
+        type: String,
+        default: 'text-violet-900 bg-violet-100 hover:bg-violet-200 focus-visible:ring-violet-500',
+    },
+    btnIconClass: {
+        type: String,
+        default: 'text-black',
+    },
+    panelClass: String,
 })
 </script>
 
