d07120eac59f0ef6765d1120c33e855661c7ccb6
MMDM <mmdehghan95@gmail.com>
2023-09-11 23:42:50 +0330

    feat: create stepy component

---
+++ frontend/src/components/base/BaseStepy.vue
@@ -0,0 +1,216 @@
+<template>
+    <div class="flex flex-wrap items-center justify-center">
+        <template
+            v-for="(step, key, idx) in steps"
+        >
+            <div
+                v-if="idx > 0"
+                class="w-2 h-2 rounded-full bg-indigo-400 m-6"
+            ></div>
+            <div
+                :class="[
+                    'rounded-full border p-3 relative my-6 flex justify-center bg-white',
+                    currStepIdx === idx ? 'border-indigo-600 bg-indigo-50' : 'border-slate-300',
+                    idx < currStepIdx ? 'bg-emerald-100' : '',
+                    allowChangeStepsByClick ? 'cursor-pointer' : ''
+                ]"
+                @click="handleStepClick(step, key, idx)"
+            >
+                <component
+                    v-if="step.icon"
+                    :is="outline[step.icon]"
+                    :class="[
+                        'w-6 h-6 transition',
+                        currStepIdx === idx ? 'text-indigo-600' : 'text-slate-400',
+                        idx < currStepIdx ? 'opacity-60 scale-90' : '',
+                    ]"
+                />
+                <outline.CheckCircleIcon
+                    v-if="idx < currStepIdx"
+                    class="w-10 h-10 text-emerald-500 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-[1]"
+                />
+                <label
+                    v-if="step.text"
+                    :class="[
+                        'absolute left-1/2 top-full translate-y-full -translate-x-1/2 text-xs whitespace-nowrap',
+                        currStepIdx === idx ? 'text-indigo-600' : '',
+                        idx > currStepIdx ? 'text-slate-500' : '',
+                    ]"
+                >{{ step.text }}</label>
+            </div>
+        </template>
+    </div>
+
+    <VTransitionSlideFadeDownY mode="out-in">
+        <template as="div"
+                  v-for="(step, idx) in Object.keys(steps)"
+                  :key="step"
+        >
+            <template v-if="simple">
+                <div
+                    v-if="currStepIdx === idx"
+                    class="mt-6"
+                >
+                    <slot
+                        :name="step"
+                        :step="steps.step"
+                        :next="handleNextClick"
+                        :prev="handlePrevClick"
+                        :currentStep="currStep"
+                        :currentStepIndex="currStepIdx"
+                        :lastStep="lastStep"
+                        :allowNextStep="allowNext"
+                        :allowPrevStep="allowPrev"
+                    ></slot>
+                </div>
+            </template>
+            <template v-else>
+                <partial-card
+                    v-if="currStepIdx === idx"
+                    class="mt-6"
+                >
+                    <template #body>
+                        <div class="p-3">
+                            <slot
+                                :name="step"
+                                :step="steps.step"
+                                :next="handleNextClick"
+                                :prev="handlePrevClick"
+                                :currentStep="currStep"
+                                :currentStepIndex="currStepIdx"
+                                :lastStep="lastStep"
+                                :allowNextStep="allowNext"
+                                :allowPrevStep="allowPrev"
+                            ></slot>
+                        </div>
+                    </template>
+                </partial-card>
+            </template>
+        </template>
+    </VTransitionSlideFadeDownY>
+
+    <partial-card
+        v-if="!manual"
+        class="mt-3"
+    >
+        <template #body>
+            <partial-stepy-next-prev-buttons
+                :current-step="currStep"
+                :current-step-index="currStepIdx"
+                :last-step="lastStep"
+                :allow-next-step="allowNext"
+                :allow-prev-step="allowPrev"
+                :loading="loading"
+                @next="handleNextClick"
+                @prev="handlePrevClick"
+                @finish="handleFinishClick"
+            />
+        </template>
+    </partial-card>
+</template>
+
+<script setup>
+import {computed, ref} from "vue";
+import * as outline from "@heroicons/vue/24/outline/index.js";
+import PartialCard from "../../components/partials/PartialCard.vue";
+import VTransitionSlideFadeDownY from "../../transitions/VTransitionSlideFadeDownY.vue";
+import PartialStepyNextPrevButtons from "../partials/PartialStepyNextPrevButtons.vue";
+
+const props = defineProps({
+    steps: Object,
+    currentStep: {
+        type: String,
+        required: true,
+    },
+    allowChangeStepsByClick: Boolean,
+    allowNextStep: {
+        type: Boolean,
+        default: true,
+    },
+    allowPrevStep: {
+        type: Boolean,
+        default: true,
+    },
+    loading: Boolean,
+    manual: Boolean,
+    simple: Boolean,
+})
+const emit = defineEmits(['next', 'prev', 'finish', 'update:currentStep', 'update:allowNextStep', 'update:allowPrevStep'])
+
+const stepsKeys = computed(() => {
+    return Object.keys(props.steps)
+})
+const currStep = computed({
+    get() {
+        return props.currentStep
+    },
+    set(value) {
+        emit('update:currentStep', value)
+    },
+})
+const currStepIdx = computed({
+    get() {
+        return stepsKeys.value.indexOf(currStep.value)
+    },
+    set(value) {
+        currStep.value = stepsKeys.value[value]
+    },
+})
+const allowNext = computed({
+    get() {
+        return props.allowNextStep
+    },
+    set(value) {
+        emit('update:allowNextStep', value)
+    },
+})
+const allowPrev = computed({
+    get() {
+        return props.allowPrevStep
+    },
+    set(value) {
+        emit('update:allowPrevStep', value)
+    },
+})
+const lastStep = computed(() => {
+    return stepsKeys.value.length - 1
+})
+
+function handleStepClick(step, key, idx) {
+    if (!props.allowChangeStepsByClick) return
+
+    if (currStepIdx.value > idx && !allowPrev.value) return
+    if (currStepIdx.value < idx && !allowNext.value) return
+
+    const res = step?.events?.click(key, idx)
+
+    if (false !== res)
+        currStepIdx.value = idx
+}
+
+function handlePrevClick() {
+    if (currStepIdx.value - 1 < 0 || !allowPrev.value) return
+
+    const res = emit('prev', currStep.value, currStepIdx.value)
+
+    if (false !== res)
+        currStepIdx.value = currStepIdx.value - 1
+}
+
+function handleNextClick() {
+    if (currStepIdx.value + 1 > lastStep.value || !allowNext.value) return
+
+    const res = emit('next', currStep.value, currStepIdx.value)
+
+    if (false !== res)
+        currStepIdx.value = currStepIdx.value + 1
+}
+
+function handleFinishClick() {
+    emit('finish', currStep.value, currStepIdx.value)
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/partials/PartialStepyNextPrevButtons.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="px-2 py-1.5 flex flex-wrap justify-end flex-col-reverse sm:flex-row relative">
+        <loader-progress
+            v-if="loading"
+            container-bg-color="!mt-0 !mb-2 bg-blue-50 opacity-70 !absolute top-0 left-1 !w-[calc(100%-.5rem)]"
+        />
+
+        <base-button
+            v-if="currentStepIndex !== 0 && showPrevStepButton"
+            :class="[
+                        '!text-black my-1.5 sm:mx-1.5 px-6 w-full sm:w-auto flex items-center group',
+                        !allowPrevStep ? '!cursor-not-allowed !text-opacity-50 border-gray-200' : 'border-gray-400 hover:bg-gray-100',
+                    ]"
+            @click="handlePrevClick"
+        >
+            <ArrowSmallRightIcon
+                :class="[
+                            'h-6 w-6 ml-auto sm:ml-2 transition',
+                            allowPrevStep ? 'group-hover:translate-x-2' : '',
+                        ]"
+            />
+            <span class="ml-auto text-sm">مرحله قبل</span>
+        </base-button>
+
+        <base-button
+            v-if="currentStepIndex !== lastStep && showNextStepButton"
+            :class="[
+                        'bg-primary text-white my-1.5 sm:mx-1.5 px-6 w-full sm:w-auto flex items-center group',
+                        !allowNextStep ? '!cursor-not-allowed !text-opacity-50 !bg-opacity-50' : '',
+                    ]"
+            @click="handleNextClick"
+        >
+            <span class="mr-auto text-sm">مرحله بعد</span>
+            <ArrowSmallLeftIcon
+                :class="[
+                            'h-6 w-6 mr-auto sm:mr-2 transition',
+                            allowNextStep ? 'group-hover:-translate-x-2' : '',
+                        ]"
+            />
+        </base-button>
+        <base-button
+            v-else
+            class="bg-emerald-500 text-white my-1.5 sm:mx-1.5 px-6 w-full sm:w-auto flex items-center group"
+            @click="handleFinishClick"
+        >
+            <span class="mr-auto text-sm">اتمام</span>
+            <CheckIcon class="h-6 w-6 mr-auto sm:mr-2 group-hover:scale-105 transition"/>
+        </base-button>
+    </div>
+</template>
+
+<script setup>
+import LoaderProgress from "../base/loader/LoaderProgress.vue";
+import BaseButton from "../base/BaseButton.vue";
+import {ArrowSmallRightIcon, ArrowSmallLeftIcon, CheckIcon} from "@heroicons/vue/24/outline/index.js";
+
+const props = defineProps({
+    currentStep: {
+        type: String,
+        required: true,
+    },
+    currentStepIndex: {
+        type: Number,
+        required: true,
+    },
+    lastStep: {
+        type: Number,
+        required: true,
+    },
+    allowNextStep: {
+        type: Boolean,
+        default: true,
+    },
+    allowPrevStep: {
+        type: Boolean,
+        default: true,
+    },
+    loading: Boolean,
+    showPrevStepButton: {
+        type: Boolean,
+        default: true,
+    },
+    showNextStepButton: {
+        type: Boolean,
+        default: true,
+    },
+})
+const emit = defineEmits(['next', 'prev', 'finish'])
+
+function handlePrevClick() {
+    if (!props.loading)
+        emit('prev')
+}
+
+function handleNextClick() {
+    if (!props.loading)
+        emit('next')
+}
+
+function handleFinishClick() {
+    if (!props.loading)
+        emit('finish')
+}
+</script>
+
+<style scoped>
+
+</style>
