d29e561c8ee02f128bbd2dc1d07f0b7dc2325049
MMDM <mmdehghan95@gmail.com>
2023-09-14 22:06:48 +0330

    fix: fix z-index

---
+++ frontend/src/components/partials/PartialBuilderRemoveBtn.vue
@@ -0,0 +1,19 @@
+<template>
+    <base-button
+        class="!absolute top-0 left-0 -translate-y-1/4 -translate-x-1/4 bg-rose-500 rounded !p-1 z-[1]"
+        @click="emit('click')"
+    >
+        <TrashIcon class="h-5 w-5"/>
+    </base-button>
+</template>
+
+<script setup>
+import {TrashIcon} from "@heroicons/vue/24/outline/index.js";
+import BaseButton from "../base/BaseButton.vue";
+
+const emit = defineEmits(['click'])
+</script>
+
+<style scoped>
+
+</style>
