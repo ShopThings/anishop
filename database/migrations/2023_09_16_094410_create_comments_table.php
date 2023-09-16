9e1e6229f55b6f16ed2af7490508c9afdc109bb8
MMDM <mmdehghan95@gmail.com>
2023-09-16 13:19:00 +0330

    feat: add product comments migration file

---
+++ database/migrations/2023_09_16_094410_create_comments_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Enums\Comments\CommentConditionsEnum;
+use App\Enums\Comments\CommentStatusesEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId('comment_id')->nullable()
+                ->constrained('comments')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->enum('condition', array_map(fn($item) => $item->name, CommentConditionsEnum::cases()))
+                ->comment('show comment condition like if it is accepted or not');
+            $table->enum('status', array_map(fn($item) => $item->name, CommentStatusesEnum::cases()))
+                ->comment('show comment status like if it is read or not');
+            $table->text('description');
+            $table->unsignedBigInteger('flag_count')->default(0);
+            $table->boolean('is_published')->default(true);
+            $table->softDeletes();
+            $table->foreignId('deleted_by')->nullable()
+                ->constrained('users')->nullOnDelete()->cascadeOnUpdate();
+            $table->timestamps();
+            $table->foreignId('created_by')->nullable()
+                ->constrained('users')->nullOnDelete()->cascadeOnUpdate();
+            $table->foreignId('updated_by')->nullable()
+                ->constrained('users')->nullOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
