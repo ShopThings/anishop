c2ce63fa1459f21bfbd51f5ed42f78a50e06c4bd
MMDM <mmdehghan95@gmail.com>
2023-09-16 13:18:34 +0330

    feat: add comment model

---
+++ app/Models/Comment.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use App\Support\Model\ExtendedModel as Model;
+use App\Support\Model\SoftDeletesTrait;
+use App\Traits\HasCreatedRelationTrait;
+use App\Traits\HasDeletedRelationTrait;
+use App\Traits\HasUpdatedRelationTrait;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Comment extends Model
+{
+    use SoftDeletesTrait,
+        HasCreatedRelationTrait,
+        HasUpdatedRelationTrait,
+        HasDeletedRelationTrait;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class, 'comment_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function children(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'comment_id');
+    }
+
+    /**
+     * @return HasMany
+     */
+    public function allChildren(): HasMany
+    {
+        return $this->children()->with('allChildren');
+    }
+}
