54671d68b9b5df5dd99b70a779d63cb1980a71cb
MMDM <mmdehghan95@gmail.com>
2023-09-25 15:08:30 +0330

    feat: add new slider option

--- app/Enums/Sliders/SliderOptionsEnum.php
+++ app/Enums/Sliders/SliderOptionsEnum.php
@@ -4,6 +4,7 @@ namespace App\Enums\Sliders;
 
 enum SliderOptionsEnum: string
 {
+    case SHOW_ALL_LINK = 'show_all_link';
     case BRAND_ID = 'brand_id';
     case CATEGORY_ID = 'category_id';
     case ORDER_BY = 'order_by';
