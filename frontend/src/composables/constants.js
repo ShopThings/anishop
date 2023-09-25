6c75c4aea85e050e5b5abd3d20ac03340ea5c33b
MMDM <mmdehghan95@gmail.com>
2023-09-25 21:14:18 +0330

    feat: add needed constants in a file

---
+++ frontend/src/composables/constants.js
@@ -0,0 +1,10 @@
+export const SLIDER_PLACES = {
+    MAIN: {
+        value: 'main',
+        text: 'اسلایدر اصلی',
+    },
+    MAIN_SLIDERS: {
+        value: 'main_sliders',
+        text: 'اسلایدرهای محصول',
+    },
+}
