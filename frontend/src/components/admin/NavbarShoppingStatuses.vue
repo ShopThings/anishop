fe1872ab0fa62bc17d4058ae4b17135f11ee4cea
MMDM <mmdehghan95@gmail.com>
2024-06-23 02:46:13 +0330

    feat: add loader and reload ability to order status show panel

--- frontend/src/components/admin/NavbarShoppingStatuses.vue
+++ frontend/src/components/admin/NavbarShoppingStatuses.vue
@@ -2,8 +2,8 @@
   <base-popover class="sm:relative" panel-klass="right-0 w-full sm:mt-3 sm:w-[25rem]">
     <template #button>
       <button
-          class="relative h-[40px] rounded-lg border-0 py-2 px-2 bg-transparent text-black hover:bg-slate-200 active:bg-slate-300 focus:bg-sky-200 transition-all flex justify-between items-center"
-          type="button"
+        class="relative h-[40px] rounded-lg border-0 py-2 px-2 bg-transparent text-black hover:bg-slate-200 active:bg-slate-300 focus:bg-sky-200 transition-all flex justify-between items-center"
+        type="button"
       >
         <ShoppingBagIcon class="h-6 w-6 "/>
         <ChevronDownIcon class="h-3 w-3 mr-1"/>
@@ -14,6 +14,31 @@
     </template>
 
     <template #panel>
+      <div class="flex flex-wrap gap-3 items-center justify-between p-2">
+        <h1 class="font-iranyekan-bold">
+          وضعیت سفارشات
+        </h1>
+
+        <base-button
+          :disabled="countingOrderStore.isLoading"
+          class="text-xs !text-black !py-0.5 border-2 hover:bg-slate-100 flex gap-2 items-center"
+          type="button"
+          @click="countingOrderStore.fetchCounting()"
+        >
+          <VTransitionFade>
+            <loader-circle
+              v-if="countingOrderStore.isLoading"
+              big-circle-color="border-transparent"
+              main-container-klass="absolute w-full h-full top-0 left-0"
+              spinner-klass="!w-6 !h-6"
+            />
+          </VTransitionFade>
+
+          <span>بارگذاری مجدد</span>
+          <ArrowPathIcon class="size-5"/>
+        </base-button>
+      </div>
+
       <div class="grid grid-cols-2 sm:grid-cols-3 gap-2.5 p-2.5">
         <div class="h-full">
           <router-link
@@ -38,9 +63,12 @@
 <script setup>
 import {inject} from "vue";
 import {ChevronDownIcon} from "@heroicons/vue/24/solid/index.js";
-import {ShoppingBagIcon} from "@heroicons/vue/24/outline/index.js";
+import {ArrowPathIcon, ShoppingBagIcon} from "@heroicons/vue/24/outline/index.js";
 import BasePopover from "@/components/base/BasePopover.vue";
 import {getTextColor, numberFormat} from "@/composables/helper.js";
+import LoaderCircle from "@/components/base/loader/LoaderCircle.vue";
+import VTransitionFade from "@/transitions/VTransitionFade.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
 
 const countingOrderStore = inject('countingOrderStore')
 </script>
