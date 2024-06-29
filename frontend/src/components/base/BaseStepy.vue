f8f3e9079a62eb9721e41643b79a504a590ae716
MMDM <mmdehghan95@gmail.com>
2024-06-29 04:38:49 +0330

    style: fix style

--- frontend/src/components/base/BaseStepy.vue
+++ frontend/src/components/base/BaseStepy.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="flex flex-wrap items-center justify-center">
     <template
-        v-for="(step, key, idx) in steps"
+      v-for="(step, key, idx) in steps"
     >
       <div
-          v-if="idx > 0"
-          class="w-2 h-2 rounded-full bg-indigo-400 m-6"
+        v-if="idx > 0"
+        class="w-2 h-2 rounded-full bg-indigo-400 m-6"
       ></div>
       <div
-          :class="[
-            'rounded-full border p-3 relative my-6 flex justify-center bg-white',
+        :class="[
+            'rounded-full border p-3 relative flex justify-center bg-white',
             currStepIdx === idx ? 'border-indigo-600 bg-indigo-50' : 'border-slate-300',
             idx < currStepIdx ? 'bg-emerald-100' : '',
-            allowChangeStepsByClick ? 'cursor-pointer' : ''
+            allowChangeStepsByClick ? 'cursor-pointer' : '',
+            stepBtnClass,
         ]"
-          @click="handleStepClick(step, key, idx)"
+        @click="handleStepClick(step, key, idx)"
       >
         <component
-            :is="outline[step.icon]"
-            v-if="step.icon"
-            :class="[
+          :is="outline[step.icon]"
+          v-if="step.icon"
+          :class="[
               'w-6 h-6 transition',
               currStepIdx === idx ? 'text-indigo-600' : 'text-slate-400',
               idx < currStepIdx ? 'opacity-60 scale-90' : '',
@@ -27,12 +28,12 @@
           ]"
         />
         <outline.CheckCircleIcon
-            v-if="idx < currStepIdx"
-            class="w-10 h-10 text-emerald-500 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-[1]"
+          v-if="idx < currStepIdx"
+          class="w-10 h-10 text-emerald-500 absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 z-[1]"
         />
         <label
-            v-if="step.text"
-            :class="[
+          v-if="step.text"
+          :class="[
               'absolute left-1/2 top-full translate-y-full -translate-x-1/2 text-xs whitespace-nowrap',
               currStepIdx === idx ? 'text-indigo-600' : '',
               idx > currStepIdx ? 'text-slate-500' : '',
@@ -45,45 +46,45 @@
 
   <VTransitionSlideFadeDownY mode="out-in">
     <template
-        is="div"
-        v-for="(step, idx) in Object.keys(steps)"
-        :key="step"
+      is="div"
+      v-for="(step, idx) in Object.keys(steps)"
+      :key="step"
     >
       <template v-if="simple">
         <div
-            v-if="currStepIdx === idx"
-            class="mt-6"
+          v-if="currStepIdx === idx"
+          :class="stepContainerClass"
         >
           <slot
-              :allowNextStep="allowNext"
-              :allowPrevStep="allowPrev"
-              :currentStep="currStep"
-              :currentStepIndex="currStepIdx"
-              :lastStep="lastStep"
-              :name="step"
-              :next="handleNextClick"
-              :prev="handlePrevClick"
-              :step="steps.step"
+            :allowNextStep="allowNext"
+            :allowPrevStep="allowPrev"
+            :currentStep="currStep"
+            :currentStepIndex="currStepIdx"
+            :lastStep="lastStep"
+            :name="step"
+            :next="handleNextClick"
+            :prev="handlePrevClick"
+            :step="steps.step"
           ></slot>
         </div>
       </template>
       <template v-else>
         <partial-card
-            v-if="currStepIdx === idx"
-            class="mt-6"
+          v-if="currStepIdx === idx"
+          :class="stepContainerClass"
         >
           <template #body>
             <div class="p-3">
               <slot
-                  :allowNextStep="allowNext"
-                  :allowPrevStep="allowPrev"
-                  :currentStep="currStep"
-                  :currentStepIndex="currStepIdx"
-                  :lastStep="lastStep"
-                  :name="step"
-                  :next="handleNextClick"
-                  :prev="handlePrevClick"
-                  :step="steps.step"
+                :allowNextStep="allowNext"
+                :allowPrevStep="allowPrev"
+                :currentStep="currStep"
+                :currentStepIndex="currStepIdx"
+                :lastStep="lastStep"
+                :name="step"
+                :next="handleNextClick"
+                :prev="handlePrevClick"
+                :step="steps.step"
               ></slot>
             </div>
           </template>
@@ -93,20 +94,20 @@
   </VTransitionSlideFadeDownY>
 
   <partial-card
-      v-if="!manual"
-      class="mt-3"
+    v-if="!manual"
+    class="mt-3"
   >
     <template #body>
       <partial-stepy-next-prev-buttons
-          :allow-next-step="allowNext"
-          :allow-prev-step="allowPrev"
-          :current-step="currStep"
-          :current-step-index="currStepIdx"
-          :last-step="lastStep"
-          :loading="loading"
-          @finish="handleFinishClick"
-          @next="handleNextClick"
-          @prev="handlePrevClick"
+        :allow-next-step="allowNext"
+        :allow-prev-step="allowPrev"
+        :current-step="currStep"
+        :current-step-index="currStepIdx"
+        :last-step="lastStep"
+        :loading="loading"
+        @finish="handleFinishClick"
+        @next="handleNextClick"
+        @prev="handlePrevClick"
       />
     </template>
   </partial-card>
@@ -143,6 +144,14 @@ const props = defineProps({
   loading: Boolean,
   manual: Boolean,
   simple: Boolean,
+  stepContainerClass: {
+    type: String,
+    default: 'mt-6',
+  },
+  stepBtnClass: {
+    type: String,
+    default: 'my-6',
+  },
 })
 const emit = defineEmits([
   'next', 'prev', 'finish',
