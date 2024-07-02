b8a7315beda4e4233584a36df65f02c2336e3e3f
MMDM <mmdehghan95@gmail.com>
2024-07-02 16:27:42 +0330

    fix: fix showing stuffs

--- app/Exports/Excels/ProductExport.php
+++ app/Exports/Excels/ProductExport.php
@@ -5,27 +5,38 @@ namespace App\Exports\Excels;
 use App\Enums\Times\TimeFormatsEnum;
 use App\Services\Contracts\ReportServiceInterface;
 use App\Support\Export\ExcelExport;
+use App\Support\Filter;
 use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
-use Psr\Container\ContainerExceptionInterface;
-use Psr\Container\NotFoundExceptionInterface;
 
-class ProductExport extends ExcelExport
+class ProductExport extends ExcelExport implements WithEvents
 {
     protected array $dateColumnsNames = ['M', 'O'];
 
     /**
-     * @return Collection
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @var Collection
      */
-    public function collection()
+    protected Collection $products;
+
+    public function __construct(array $query, Filter $filter)
     {
+        parent::__construct($query, $filter);
+
         /**
          * @var ReportServiceInterface $service
          */
         $service = app()->get(ReportServiceInterface::class);
-        return $service->getProductsForReport($this->filter, $this->query);
+        $this->products = $service->getProductsForReport($this->filter, $this->query);
+    }
+
+    /**
+     * @return Collection
+     */
+    public function collection()
+    {
+        return $this->products;
     }
 
     /**
@@ -46,22 +57,22 @@ class ProductExport extends ExcelExport
         foreach ($products as $product) {
             $productsStr .= '(' . "\n";
             $productsStr .= 'کد: ' . $product->code . "\n";
-            $productsStr .= 'رنگ: ' . $product->color_name ?? '-' . "\n";
-            $productsStr .= 'سایز: ' . $product->size ?? '-' . "\n";
-            $productsStr .= 'گارانتی: ' . $product->guarantee . "\n";
+            $productsStr .= 'رنگ: ' . ($product->color_name ?? '-') . "\n";
+            $productsStr .= 'سایز: ' . ($product->size ?? '-') . "\n";
+            $productsStr .= 'گارانتی: ' . ($product->guarantee ?? '-') . "\n";
             $productsStr .= 'وزن با بسته‌بندی (به گرم): ' . $this->formatNumber($product->weight, 0) . "\n";
             $productsStr .= 'قیمت (به تومان): ' . $this->formatNumber($product->price) . "\n";
             $productsStr .= 'قیمت با تخفیف (به تومان): ' . $this->formatNumber($product->discounted_price, '-') . "\n";
             $productsStr .= 'تخفیف از تاریخ: ' . ($product->discounted_from ? vertaTz($row->discounted_until)->format(TimeFormatsEnum::NORMAL_DATETIME->value) : '-') . "\n";
             $productsStr .= 'تخفیف تا تاریخ: ' . ($product->discounted_until ? vertaTz($row->discounted_until)->format(TimeFormatsEnum::NORMAL_DATETIME->value) : '-') . "\n";
-            $productsStr .= 'درصد اعمال مالیات: ' . $product->tax_rate . "\n";
+            $productsStr .= 'درصد اعمال مالیات: ' . ($product->tax_rate ?: 0) . "\n";
             $productsStr .= 'تعداد موجود در انبار: ' . $this->formatNumber($product->stock_count) . "\n";
-            $productsStr .= 'محصول ویژه: ' . (bool)$product->is_special . "\n";
-            $productsStr .= 'وضعیت موجودی: ' . (bool)$product->is_available . "\n";
-            $productsStr .= 'نمایش برچسب به زودی: ' . (bool)$product->show_coming_soon . "\n";
-            $productsStr .= 'نمایش برچسب تماس برای اطلاعات بیشتر: ' . (bool)$product->show_call_for_more . "\n";
-            $productsStr .= 'وضعیت انتشار: ' . (bool)$product->is_published . "\n";
-            $productsStr .= 'مرسوله مجزا: ' . (bool)$product->has_separate_shipment . "\n";
+            $productsStr .= 'محصول ویژه: ' . ($product->is_special ? '✅' : '❌') . "\n";
+            $productsStr .= 'وضعیت موجودی: ' . ($product->is_available ? '✅' : '❌') . "\n";
+            $productsStr .= 'نمایش برچسب به زودی: ' . ($product->show_coming_soon ? '✅' : '❌') . "\n";
+            $productsStr .= 'نمایش برچسب تماس برای اطلاعات بیشتر: ' . ($product->show_call_for_more ? '✅' : '❌') . "\n";
+            $productsStr .= 'وضعیت انتشار: ' . ($product->is_published ? '✅' : '❌') . "\n";
+            $productsStr .= 'مرسوله مجزا: ' . ($product->has_separate_shipment ? '✅' : '❌') . "\n";
             $productsStr .= ')' . "\n";
             $productsStr .= "\r\n";
         }
@@ -76,7 +87,7 @@ class ProductExport extends ExcelExport
             $row->unit_name,
             implode(', ', $row->keywords),
             (bool)$row->is_available,
-            (bool)$row->is_commenting_allwed,
+            (bool)$row->is_commenting_allowed,
             (bool)$row->is_published,
             $productsStr,
             $row->created_at
@@ -100,14 +111,16 @@ class ProductExport extends ExcelExport
      */
     private function prepareSubPropertiesForShow(array $properties): string
     {
-        if (empty($properties)) return '';
+        if (empty($properties)) return '-';
 
         $builder = '';
         foreach ($properties as $property) {
             $builder .= $property['title'] . ':' . "\n";
             $builder .= implode(', ', $property['tags']) . "\n";
         }
-        return $builder;
+        $builder = trim($builder);
+
+        return !empty($builder) ? $builder : '-';
     }
 
     /**
@@ -116,14 +129,16 @@ class ProductExport extends ExcelExport
      */
     private function preparePropertiesForShow(array $properties): string
     {
-        if (empty($properties)) return '';
+        if (empty($properties)) return '-';
 
         $builder = '';
         foreach ($properties as $property) {
             $builder .= $property['title'] . ':' . "\n";
             $builder .= $this->prepareSubPropertiesForShow($property['children']) . "\n";
         }
-        return $builder;
+        $builder = trim($builder);
+
+        return !empty($builder) ? $builder : '-';
     }
 
     /**
@@ -142,7 +157,7 @@ class ProductExport extends ExcelExport
                 'واحد محصول',
                 'کلمات کلیدی',
                 'وضعیت موجودی',
-                'اجازه ارسال کامنت',
+                'اجازه ارسال دیدگاه',
                 'وضعیت انتشار',
                 'انواع محصولات',
                 'تاریخ ایجاد',
@@ -160,7 +175,7 @@ class ProductExport extends ExcelExport
                 'Unit Name',
                 'Keywords',
                 'Availability',
-                'Allowed Commenting',
+                'Commenting Allowed',
                 'Publish Status',
                 'Product Variants',
                 'Created At',
@@ -168,6 +183,27 @@ class ProductExport extends ExcelExport
                 'Updated At',
                 'Updated At (Asia/Tehran)',
             ],
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet;
+                $lastRow = $sheet->getHighestRow();
+
+                // Add total row
+                $totalRow = $lastRow + 1;
+
+                $this->addContentToSpecificRow($sheet, $totalRow, [
+                    'B' => 'تعداد کل محصولات',
+                    'C' => $this->products->count(),
+                ]);
+            },
         ];
     }
 }
