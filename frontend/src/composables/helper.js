f6d51ec65f7be690323c8a0ed82fafa4538bc661
MMDM <mmdehghan95@gmail.com>
2024-07-09 17:52:13 +0330

    feat: add new array equal check

--- frontend/src/composables/helper.js
+++ frontend/src/composables/helper.js
@@ -184,6 +184,29 @@ export const getTextColor = (backgroundColor) => {
   return brightness > 155 ? 'black' : 'white';
 }
 
+/**
+ * @see https://stackoverflow.com/a/16436975/12154893
+ *
+ * @param a
+ * @param b
+ * @returns {boolean}
+ */
+export const arraysEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+  // Please note that calling sort on an array will modify that array.
+  // you might want to clone your array first.
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
 export const nestedArray = {
   get(obj, key) {
     return key.split('.').reduce((acc, currentKey) => acc ? acc[currentKey] : undefined, obj)
